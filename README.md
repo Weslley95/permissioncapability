fbf60fe71f4e87f86b12f860268922e4eb46e766
Weslley da Penha Bezerra <weslley@MacBook-Pro-de-Weslley.local>
2021-12-08 11:08:01 -0300

    Plugin local for store  capabilities

---
+++ README.md
@@ -0,0 +1,33 @@
+## Permission Capability
+
+## ⚙️ Functionality
+
+- [x] Add new capacity rules
+- [x] Allocate your capabilities in one place
+- [x] Add new capacity rules
+- [x] There is a capability dedicated to viewing some categories of courses
+
+## 🛠 How to use
+
+1. Clone the repository to your theme folder
+<pre>
+https://github.com/Weslley95/permissioncapability.git
+</pre>
+
+2. Allocate the folder, in the local moodle folder
+
+<pre>
+moodle/
+├── ...                                
+├── local/                            
+|   ├── permissioncapability/                         
+|   │   ├── files and folders/                                                           
+|   |   └── ...                        
+│   └── ...                            
+└── ...                                
+</pre>
+
+💡 Use this plugin with a version above 3.8.
+
+## 📝 Licença
+Licença [MIT](./LICENSE).
---
+++ lang/pt_br/local_permissioncapability.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local plugin "permissioncapability" - Language pack
+ *
+ * @package    local_permissioncapability
+ * @copyright  2021 Weslley <weslleybezerra95@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Permissão de Capacidade';
+
+// Module category
+$string['permissioncapability:viewvisiblecategories'] = 'Ver categoria visível';
+$string['local/permissioncapability:visible'] = 'Ver categoria visível';
